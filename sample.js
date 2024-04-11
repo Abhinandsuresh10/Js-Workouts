50c688ae2e9f4aab153d85d1a5808d40ac441c69
Vishnu Mohan <vishnumohanmfc@gmail.com>
2024-04-11 20:30:19 +0530

    Palindrome

--- sample.js
+++ sample.js
@@ -1,19 +1,44 @@
 //-----Sort 'a' named friuts and capitalize 'A' -----//
-const fruits = ["apple", "orange", "anar", "grape"];
-let newFruits = [];
-let arr = [];
+// const fruits = ["apple", "orange", "anar", "grape"];
+// let newFruits = [];
+// let arr = [];
 
-for (let i = 0; i < fruits.length; i++){
-    if (fruits[i].charAt(0) == 'a') {
-        newFruits.push(fruits[i]);
+// for (let i = 0; i < fruits.length; i++){
+//     if (fruits[i].charAt(0) == 'a') {
+//         newFruits.push(fruits[i]);
+//     }
+// };
+
+// for (let key of newFruits) { //for of loop
+//     let updatedKey = key.charAt(0).toUpperCase() + key.slice(1);
+//     arr.push(updatedKey)
+// }
+
+// console.log(newFruits);
+// console.log(arr);
+//-------------------------------------------------------------------------//
+
+
+//-----Palindrome with loop-----//
+let str = 'malayalam';
+let isPalindrome = true;
+
+let leftIndex = 0;
+let rightIndex = str.length-1;
+
+for (let i = 0; i < str.length / 2; i++){
+    if (str[leftIndex] !== str[rightIndex]) {
+        isPalindrome = false;
+        break;
     }
-};
-
-for (let key of newFruits) { //for of loop
-    let updatedKey = key.charAt(0).toUpperCase() + key.slice(1);
-    arr.push(updatedKey)
+    leftIndex++;
+    rightIndex--;
 }
 
-console.log(newFruits);
-console.log(arr);
+if (isPalindrome) {
+    console.log(`${str} is palindrome`);
+} else {
+    console.log(`${str} is not palindrome`);
+}
 //-------------------------------------------------------------------------//
+
