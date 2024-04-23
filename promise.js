462b9203cfa1759362804020bf6fadbb66a8915d
Vishnu Mohan <vishnumohanmfc@gmail.com>
2024-04-23 14:36:52 +0530

    Loops

--- promise.js
+++ promise.js
@@ -1,16 +1,10 @@
-// Create a new Promise object
 let promise = new Promise((resolve, reject) => {
-    // Log a message indicating that we're working with a Promise
-    console.log("I am a promise");
-    // Resolve the Promise with a value of 2255
-    resolve(2255);
+    console.log('Iam a promise');
+    resolve(200);
 });
 
-// Attach a function to handle the successful resolution of the Promise
 promise.then((value) => {
-    // Log a message along with the resolved value
-    console.log('Resolved:', value);
-}).catch((error) => {
-    // If something goes wrong, log an error message
-    console.log('Rejected:', error);
+    console.log('Resolved', value);
+}).catch((err) => {
+    console.log('Failed');
 });
